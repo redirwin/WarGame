f2bb7cbf1042406ed25eb9922b1bdbba12eed96d
Dave Irwin <david@redirwin.com>
2020-03-06 20:38:24 -0700

    Removed instantiation of fields.

--- Card.java
+++ Card.java
@@ -2,24 +2,29 @@ package WarGame;
 
 public class Card {
 	
-	int[] value = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-	String[] name = {"Diamonds", "Hearts", "Clubs", "Spades"};
+	private int value;
+	private String name;
 	
 	public static void describe(int value, String name) {
 		System.out.println("Card: " + value + " of " + name);
-	}	
-	
-	public int[] getValue() {
+	}
+
+	public int getValue() {
 		return value;
 	}
-	public void setValue(int[] value) {
+
+	public void setValue(int value) {
 		this.value = value;
 	}
-	public String[] getName() {
+
+	public String getName() {
 		return name;
 	}
-	public void setName(String[] name) {
+
+	public void setName(String name) {
 		this.name = name;
 	}	
+	
+		
 
 }
